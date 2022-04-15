405a531c53ed21215c9f888b1956d12d00cf1e84
Aaron Elias <aaronelias007@gmail.com>
2022-04-14 21:53:39 -0400

    Yolo

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -3,11 +3,22 @@ import PokemonCard from "./PokemonCard";
 import { Card } from "semantic-ui-react";
 
 function PokemonCollection({pokemon}) {
-  return (
-    <Card.Group itemsPerRow={6}>
-      <PokemonCard />
-    </Card.Group>
-  );
+  const cards = pokemon.map((poke) => (
+    <PokemonCard key={poke.id} pokemon={poke} /> 
+  ));
+ 
+
+    return <Card.Group itemsPerRow={6}>{cards}</Card.Group>
+
 }
 
 export default PokemonCollection;
+
+
+// function PokemonCollection({ pokemon }) {
+//   const cards = pokemon.map((poke) => (
+//     <PokemonCard key={poke.id} pokemon={poke} />
+//   ));
+
+//   return <Card.Group itemsPerRow={6}>{cards}</Card.Group>;
+// }
