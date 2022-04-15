8c7ca870ba1d7d6a35039d1ae5e4ae43e9ddbc4c
Aaron Elias <aaronelias007@gmail.com>
2022-04-14 21:34:18 -0400

    checking chain

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -10,7 +10,7 @@ const [pokemon, setPokemon] = useState([])
 useEffect(() => {
   fetch("http://localhost:3001/pokemon")
     .then((r) => r.json())
-    .then((pokemon) => setPokemon(pokemon));
+    .then(setPokemon);
 }, []);
 
 
