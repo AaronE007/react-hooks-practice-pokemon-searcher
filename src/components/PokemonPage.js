d582c9f84ecc446d2c2c830abf3dce2a0231d704
Aaron Elias <aaronelias007@gmail.com>
2022-04-14 21:26:28 -0400

    fetch template

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonCollection from "./PokemonCollection";
 import PokemonForm from "./PokemonForm";
 import Search from "./Search";
 import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
+const [pokemon, setPokemon] = useState([])
+
+useEffect(() => {
+  fetch("http://localhost:3001/pokemon")
+    .then((r) => r.json())
+    .then((items) => console.log(items));
+}, []);
+
+
   return (
     <Container>
       <h1>Pokemon Searcher</h1>
