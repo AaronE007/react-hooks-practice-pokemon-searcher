f68406d041362fb2933304de3b41354f58f8bb33
Aaron Elias <aaronelias007@gmail.com>
2022-04-16 21:00:39 -0400

    finished search, going to add poke

--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -6,13 +6,21 @@ import { Container } from "semantic-ui-react";
 
 function PokemonPage() {
 const [pokemon, setPokemon] = useState([])
-const [searchTerm, setSearchTerm] =useState("")
+const [searchTerm, setSearchTerm] = useState("")
 
 useEffect(() => {
   fetch("http://localhost:3001/pokemon")
     .then((r) => r.json())
     .then(setPokemon);
 }, []);
+
+  const pokemonsToDisplay = pokemon.filter((poke) =>
+    poke.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  function handleAddPokemon(newPokemon) {
+        setPokemon([...pokemon, newPokemon]);
+      }
 
 
   return (
@@ -21,9 +29,9 @@ useEffect(() => {
       <br />
       <PokemonForm />
       <br />
-      <Search />
+      <Search searchTerm={searchTerm} onChangeSearch={setSearchTerm} />
       <br />
-      <PokemonCollection pokemon={pokemon} />
+      <PokemonCollection pokemon={pokemonsToDisplay} />
     </Container>
   );
 }
